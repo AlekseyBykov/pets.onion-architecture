4b2d7675caff331893aec1b7842a6f1bad4451ae
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-10-04 10:09:16 +0400

    Registered custom psql function. Added Postgres driver and additional configuration.

--- domain/src/main/java/alekseybykov/portfolio/component/exceptions/EntityNotFoundException.java
+++ domain/src/main/java/alekseybykov/portfolio/component/exceptions/EntityNotFoundException.java
@@ -1,5 +1,13 @@
+//
+// Feel free to use these solutions in your work.
+//
 package alekseybykov.portfolio.component.exceptions;
 
+/**
+ * @author  aleksey.n.bykov@gmail.com
+ * @version 1.0
+ * @since   2019-10-03
+ */
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException() { }
--- domain/src/main/java/alekseybykov/portfolio/component/listeners/AuditListener.java
+++ domain/src/main/java/alekseybykov/portfolio/component/listeners/AuditListener.java
@@ -1,3 +1,6 @@
+//
+// Feel free to use these solutions in your work.
+//
 package alekseybykov.portfolio.component.listeners;
 
 import alekseybykov.portfolio.component.audit.AuditManager;
