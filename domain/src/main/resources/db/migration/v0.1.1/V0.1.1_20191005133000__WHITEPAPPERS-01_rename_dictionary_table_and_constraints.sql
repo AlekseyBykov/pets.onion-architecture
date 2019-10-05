70aae4310df5c6e69590a3ccd7b6b0ad6c022ee6
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-10-05 14:04:34 +0400

    Added some corrections to migration according to renaming.

--- domain/src/main/resources/db/migration/v0.1.1/V0.1.1_20191005133000__WHITEPAPPERS-01_rename_dictionary_table_and_constraints.sql
+++ domain/src/main/resources/db/migration/v0.1.1/V0.1.1_20191005133000__WHITEPAPPERS-01_rename_dictionary_table_and_constraints.sql
@@ -3,8 +3,11 @@ alter table white_papper_metadata drop constraint if exists white_papper_metadat
 alter table if exists scope rename to some_dictionary;
 
 comment on table some_dictionary is 'Example of some dictionary';
-comment on column some_dictionary.id is 'Dictionary item identifier'
+comment on column some_dictionary.id is 'Dictionary item identifier';
 comment on column some_dictionary.name is 'Dictionary item name';
+
+alter table white_papper_metadata drop column scope_id;
+alter table white_papper_metadata add column some_dictionary_id bigint;
 
 alter table white_papper_metadata add constraint white_papper_metadata_some_dictionary_id_fkey
     foreign key (some_dictionary_id) references some_dictionary (id);
